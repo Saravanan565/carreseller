fc9d74c48f3f22994180268f65536d60c4d22660
Saravanan565 <117788442+Saravanan565@users.noreply.github.com>
2024-01-01 21:54:43 +0530

    Add files via upload

---
+++ Controllers/returncarController.cs
@@ -0,0 +1,75 @@
+﻿using rentcar.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rentcar.Controllers
+{
+    public class returncarController : Controller
+    {
+
+        rentcarEntities1 db = new rentcarEntities1();
+        // GET: returncar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(returncar recar)
+        {
+            if(ModelState.IsValid) 
+            {
+                db.returncars.Add(recar);
+
+                var car = db.carregs.SingleOrDefault(e=> e.carno == recar.carno);
+
+                if(car == null)
+                 return HttpNotFound("CarNo not Found");
+                car.available = "yes";
+                db.Entry(car).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(recar);
+        }
+
+
+
+
+
+
+
+
+
+
+
+        [HttpPost]
+        public ActionResult Getid(String carno)
+        {
+            var carn = (from s in db.rentails
+                        where s.carid == carno
+                        select new
+                        {
+                            StartDate = s.sdate,
+                            EndDate = s.edate,
+                            Custid = s.custid,
+                            CarNo = s.carid,
+                            Fee = s.fee,
+                            ElapsedDays = SqlFunctions.DateDiff("day", s.edate, DateTime.Now)
+
+
+                        }).ToArray();
+            return Json(carn, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+    }
+}
